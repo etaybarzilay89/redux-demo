d7537bbec0ad1e7b95416bef5de9df0c012e8a32
orenh <orenh@wix.com>
2017-06-13 11:05:50 +0300

    initial

---
+++ src/index.js
@@ -0,0 +1,31 @@
+import { createStore } from 'redux'
+
+const INC = 'inc'
+
+const increase = () => ({ type: INC })
+
+const defaultState = { count: 0 }
+
+const reducer = (state = defaultState, action) => 
+  action.type === INC ? { count: state.count + 1 } : state  
+
+const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+
+const button = document.createElement('button')
+const display = document.createElement('div')
+
+document.body.appendChild(button)
+button.innerText = 'Increment'
+
+document.body.appendChild(display)
+
+const updateUI = () => {
+  display.innerText = store.getState().count  
+}
+store.subscribe(updateUI)
+
+button.addEventListener('click', () => {
+  store.dispatch(increase())
+})
+
+updateUI()
