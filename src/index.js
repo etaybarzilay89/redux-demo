11a5f196d736dced189635024c26372f7661bf2f
orenh <orenh@wix.com>
2017-06-13 16:15:25 +0300

    with reselect

--- src/index.js
+++ src/index.js
@@ -1,70 +1,87 @@
-import { createStore, combineReducers } from 'redux'
+import { createStore } from 'redux'
 import React from 'react'
 import { render } from 'react-dom'
 import { Provider, connect } from 'react-redux'
+import { pure } from 'recompose'
+import { createSelector } from 'reselect'
+import { map, toPairs } from 'lodash/fp'
 
 const INC = 'inc'
+const SOME_ACTION = 'some-action'
 
 const increase = counterName => ({ type: INC, counterName })
+const someAction = () => ({type: SOME_ACTION})
 
-const selectCount = counterName => state => state[counterName]
+const selectCounts = state => state.counts
 
-const countReducer = counterName => (count = 0, action) => 
-  action.type === INC && counterName === action.counterName ? count + 1 : count
+const selectCounters = state => map(([name, count]) => ({name, count}), toPairs(state.counts))
+// const selectCounters = createSelector(
+//   selectCounts,
+//   counts => map(([name, count]) => ({name, count}), toPairs(counts))
+// )
 
-const reducer = combineReducers({
-  count1: countReducer('count1'),
-  count2: countReducer('count2'),
-  count3: countReducer('count3')
-})
+const defaultState = {
+  someState: 0,
+  counts: {a: 0, b: 0, c: 0} 
+}
+
+const reducer = (state = defaultState, action) => {
+  switch(action.type){
+    case INC: 
+      return {...state, counts: {...state.counts, [action.counterName]: state.counts[action.counterName] + 1}}
+    case SOME_ACTION:
+      console.log('some action')
+      return {...state, someState: state.someState + 1 }
+    default:
+      return state
+  }
+}
 
 const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 
 const Increase = ({onIncrease}) => 
   <button onClick={onIncrease}>Increase</button>
 
-const Display = ({count}) => 
+const Display = pure(({count}) => 
   <div>{ count }</div>
+)
 
-const Counter = ({count, onIncrease}) => 
-  <div>
-    <Increase onIncrease={onIncrease}/>
-    <Display count={count}/>
+const Counters = pure(({counts, onIncrease}) => {
+  console.log(counts)
+  return <div>
+    {
+      map(({name, count}) => {
+        return <div key={name}>
+          <Increase onIncrease={() => onIncrease(name)}/>
+          <Display count={count}/>
+        </div>
+      }, 
+      counts)
+    }
   </div>
+}
+)
 
-const mapStateToProps = counterName => state => ({
-  count: selectCount(counterName)(state)
+const mapStateToProps = state => ({
+  counts: selectCounters(state)
 })
 
-const mapDispatchToProps = counterName => dispatch => ({
-  onIncrease: () => dispatch(increase(counterName))
+const mapDispatchToProps = dispatch => ({
+  onIncrease: (counterName) => dispatch(increase(counterName))
 })
 
-const SmartCounter1 = connect(mapStateToProps('count1'), mapDispatchToProps('count1'))(Counter)
-const SmartCounter2 = connect(mapStateToProps('count2'), mapDispatchToProps('count2'))(Counter)
-const SmartCounter3 = connect(mapStateToProps('count3'), mapDispatchToProps('count3'))(Counter)
-
-const Sum = connect(state => ({
-  count: selectCount('count1')(state) + selectCount('count2')(state)
-}))(Display)
+const SmartCounters = connect(mapStateToProps, mapDispatchToProps)(Counters)
 
 const App = () => 
   <div>
-    <SmartCounter1/>
-    <SmartCounter2/>
-    <SmartCounter3/>
-    <Sum/>
+    <button onClick={() => store.dispatch(someAction())}>Some Action</button>
+    <SmartCounters/>
   </div>
 
-const updateUI = () => {
-  render(
-    <Provider store={store}>
-      <App/>
-    </Provider>, 
-    document.getElementById('root')
-  )
-}
 
-store.subscribe(updateUI)
-
-updateUI()
+render(
+  <Provider store={store}>
+    <App/>
+  </Provider>, 
+  document.getElementById('root')
+)
